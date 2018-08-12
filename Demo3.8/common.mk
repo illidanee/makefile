f9cc0a9a1005941662a18d4008eaec47bbec66c1
illidan <illidan@illidan.org>
2018-08-11 23:21:34 -0400

    使用动态库

--- Demo3.8/common.mk
+++ Demo3.8/common.mk
@@ -15,6 +15,10 @@ $(shell mkdir -p $(LIB_OJBS_DIR))
 ifneq ("$(LIB)", "")
 BIN_OBJS_DIR := $(LIB_OJBS_DIR)
 endif
+ifneq ("$(DLL)", "")
+BIN_OBJS_DIR := $(LIB_OJBS_DIR)
+DLL_FLAG := -fPIC
+endif
 
 #  ---------- Vars
 
@@ -27,6 +31,7 @@ DEPS := $(addprefix $(BIN_DEPS_DIR)/,$(DEPS))
 OBJS := $(addprefix $(BIN_OBJS_DIR)/,$(OBJS))
 
 LIB  := $(addprefix $(BIN_LIBS_DIR)/,$(LIB))
+DLL  := $(addprefix $(BIN_LIBS_DIR)/,$(DLL))
 
 #  ---------- Global Dirs And Vars
 
@@ -34,12 +39,16 @@ ALL_OBJS := $(wildcard $(BIN_OBJS_DIR)/*.o)
 ALL_OBJS += $(OBJS)
 ALL_LIBS := $(wildcard $(BIN_LIBS_DIR)/*.a)
 ALL_LIBS += $(LIB)
+ALL_DLLS := $(wildcard $(BIN_LIBS_DIR)/*.so)
+ALL_DLLS += $(DLL)
 
 ALL_LIBS_NAME := $(patsubst lib%,-l%,$(basename $(notdir $(ALL_LIBS))))
+ALL_DLLS_NAME := $(patsubst lib%,-l%,$(basename $(notdir $(ALL_DLLS)))) 
+ALL_NAME      := $(ALL_LIBS_NAME) $(ALL_DLLS_NAME)
 
 #  ---------- Rulers
 
-Default:$(DEPS) $(OBJS) $(LIB) $(BIN)
+Default:$(DEPS) $(OBJS) $(LIB) $(DLL) $(BIN)
 	@echo "--Good Sub Job!"
 
 ifneq ("$(wildcard $(DEPS))", "")
@@ -50,13 +59,16 @@ $(BIN_DEPS_DIR)/%.d:%.c
 	gcc -I$(BIN_INCS_DIR) -MM $(filter %.c, $^) | sed 's,\(.*\)\.o[ :]*,$(BIN_OBJS_DIR)/\1\.o $@:,g'> $@
 
 $(BIN_OBJS_DIR)/%.o:%.c
-	gcc -I$(BIN_INCS_DIR) -o $@ -c $(filter %.c, $^)
+	gcc $(DLL_FLAG) -I$(BIN_INCS_DIR) -o $@ -c $(filter %.c, $^)
 
 $(LIB):$(OBJS)
 	ar rcs $@ $^
 
-$(BIN):$(ALL_OBJS) $(ALL_LIBS)
-	gcc -o $@ $^ -L$(BIN_LIBS_DIR) $(ALL_LIBS_NAME) 
+$(DLL):$(OBJS)
+	gcc -shared -o $@ $^
+
+$(BIN):$(ALL_OBJS) $(ALL_LIBS) $(ALL_DLLS)
+	gcc -o $@ $^ -L$(BIN_LIBS_DIR) $(ALL_NAME) 
 
 #  ---------- PHONYS
 
@@ -65,14 +77,18 @@ print:
 	@echo "BIN_DEPS_DIR:$(BIN_DEPS_DIR)"
 	@echo "BIN_OBJS_DIR:$(BIN_OBJS_DIR)"
 	@echo "LIB_OJBS_DIR:$(LIB_OJBS_DIR)"
+	@echo "ALL_NAME:$(ALL_NAME)"
+	@echo "ALL_LIBS_NAME:$(ALL_LIBS_NAME)"
+	@echo "ALL_DLLS_NAME:$(ALL_DLLS_NAME)"
 	@echo "SRCS : $(SRCS)" 
 	@echo "DEPS : $(DEPS)"
 	@echo "OBJS : $(OBJS)"
 	@echo "LIB  : $(LIB)"
+	@echo "DLL  : $(DLL)"
 	@echo "BIN  : $(BIN)"
 
 clean:
-	rm -f $(DEPS) $(OBJS) $(LIB) $(BIN)
+	rm -f $(DEPS) $(OBJS) $(LIB) $(DLL) $(BIN)
 
 
 
